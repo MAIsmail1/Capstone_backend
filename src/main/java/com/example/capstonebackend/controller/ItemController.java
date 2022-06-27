ac3753dfd115228f4724794ba7902b929ca1eb60
hufengyi11 <fengyihu456@gmail.com>
2022-06-27 22:58:44 +0100

    so far so good

--- src/main/java/com/example/capstonebackend/controller/ItemController.java
+++ src/main/java/com/example/capstonebackend/controller/ItemController.java
@@ -1,8 +1,40 @@
 package com.example.capstonebackend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.capstonebackend.model.Item;
+import com.example.capstonebackend.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ItemController {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @GetMapping("/items")
+    public ResponseEntity<List<Item>> getAllItems(){
+        List<Item> items = itemRepository.findAll();
+        return ResponseEntity.ok().body(items);
+    }
+
+    @PostMapping("/items")
+    public ResponseEntity<Item> createItem(@RequestBody Item item){
+        Item result = itemRepository.save(item);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/items/{id}")
+    public String deleteById(@PathVariable Long id){
+        itemRepository.deleteById(id);
+        return "Item deleted";
+    }
+
+
 
 }
--- src/main/java/com/example/capstonebackend/repository/CatalogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.capstonebackend.repository;
-
-import com.example.capstonebackend.model.Catalog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatalogRepository extends JpaRepository<Catalog, Long> {
-}
