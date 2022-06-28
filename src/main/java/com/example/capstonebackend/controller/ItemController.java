5c7ba45f3f3affacbc28ea17d41fd0f87fa0b858
hufengyi11 <fengyihu456@gmail.com>
2022-06-28 15:00:10 +0100

    removed some spaces

--- src/main/java/com/example/capstonebackend/controller/ItemController.java
+++ src/main/java/com/example/capstonebackend/controller/ItemController.java
@@ -1,16 +1,10 @@
 package com.example.capstonebackend.controller;
-
 import com.example.capstonebackend.model.Item;
 import com.example.capstonebackend.repository.ItemRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
