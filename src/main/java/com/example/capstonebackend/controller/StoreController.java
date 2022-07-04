cdee91159cb3c753312f5311ca30097d3600053b
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-07-04 09:50:28 +0100

    updated store repo and controller

--- src/main/java/com/example/capstonebackend/controller/StoreController.java
+++ src/main/java/com/example/capstonebackend/controller/StoreController.java
@@ -13,25 +13,43 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class StoreController {
-        @Autowired
-        private StoreRepository storeRepository;
+    @Autowired
+    private StoreRepository storeRepository;
 
-        @GetMapping("/stores")
-        public ResponseEntity<List<Store>> getAllStores(){
-            List<Store> stores = storeRepository.findAll();
-            return ResponseEntity.ok().body(stores);
-        }
+    @GetMapping("/stores")
+    public ResponseEntity<List<Store>> getAllStores() {
+        List<Store> stores = storeRepository.findAll();
+        return ResponseEntity.ok().body(stores);
+    }
 
-        @PostMapping("/stores")
-        public ResponseEntity<Store> createItem(@RequestBody Store store){
-            Store result = storeRepository.save(store);
-            return ResponseEntity.ok().body(result);
-        }
+    @GetMapping("/stores/Parachute")
+    public ResponseEntity<List<Store>> getAllParachute() {
+        List<Store> parachute = storeRepository.findAllParachute();
+        return ResponseEntity.ok().body(parachute);
+    }
 
-        @DeleteMapping("/stores/{id}")
-        public String deleteById(@PathVariable Long id){
-            storeRepository.deleteById(id);
-            return "Store deleted";
-        }
+    @GetMapping("/stores/OnePoundStore")
+    public ResponseEntity<List<Store>> getAllOnePoundStore() {
+        List<Store> onepoundstore = storeRepository.findAllOnePoundStore();
+        return ResponseEntity.ok().body(onepoundstore);
+    }
+
+    @GetMapping("/stores/EalingFoodandWine")
+    public ResponseEntity<List<Store>> getAllEalingFoodandWine() {
+        List<Store> ealingfoodandwine = storeRepository.findAllEalingFoodandWine();
+        return ResponseEntity.ok().body(ealingfoodandwine);
+    }
+
+    @PostMapping("/stores")
+    public ResponseEntity<Store> createItem(@RequestBody Store store) {
+        Store result = storeRepository.save(store);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/stores/{id}")
+    public String deleteById(@PathVariable Long id) {
+        storeRepository.deleteById(id);
+        return "Store deleted";
+    }
 
 }
--- src/main/java/com/example/capstonebackend/repository/StoreRepository.java
+++ src/main/java/com/example/capstonebackend/repository/StoreRepository.java
@@ -1,7 +1,21 @@
 package com.example.capstonebackend.repository;
 
+import com.example.capstonebackend.model.Item;
 import com.example.capstonebackend.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    @Query(value = "SELECT * FROM STORE WHERE NAME = 'Parachute'", nativeQuery = true)
+    List<Store> findAllParachute();
+
+    @Query(value = "SELECT * FROM STORE WHERE NAME  = 'One Pound Store'", nativeQuery = true)
+    List<Store> findAllOnePoundStore();
+
+    @Query(value = "SELECT * FROM STORE WHERE NAME = 'Ealing Food and Wine'", nativeQuery = true)
+    List<Store> findAllEalingFoodandWine();
+
 }
